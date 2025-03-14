e7f4ba03b9cb0ea227fa31fde7d5d631ca02cc01
yangjiwon <rennes91@daum.net>
2025-03-14 12:47:20 +0900

    test commit

--- src/main/java/com/food/map/test/TestEntity.java
+++ src/main/java/com/food/map/test/TestEntity.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 public class TestEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	@Column(name = "name")
